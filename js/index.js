f2d091f572275e8d7345708ec064ab584a00f06b
campbell18 <campbell.foskin@gmail.com>
2016-09-11 15:15:57 +1200

    Intial commit. Basic API functionality.

---
+++ js/index.js
@@ -0,0 +1,51 @@
+var retrievedQuote = "";
+var retrievedAuthor = "";
+var retrievedCategory = "movies";
+var translatedQuote = "";
+function generateQuote() {
+    getQuote();
+}
+function getQuote() {
+    console.log("Getting quote...");
+    $.ajax({
+        url: "https://andruxnet-random-famous-quotes.p.mashape.com/?cat=" + retrievedCategory,
+        beforeSend: function (xhrObj) {
+            xhrObj.setRequestHeader("Accept", "application/json");
+            xhrObj.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+            xhrObj.setRequestHeader("X-Mashape-Authorization", "AwbgEZ1Wqqmshirsyf5x49g4qTj3p1KK0uHjsnwSyoj4AQc1p5");
+        },
+        type: "POST"
+    })
+        .done(function (data) {
+        var json = JSON.parse(data);
+        retrievedQuote = json.quote;
+        console.log("Got the quote:" + retrievedQuote);
+        console.log(data);
+        getYodaSpeak();
+    })
+        .fail(function (error) {
+        console.log(error);
+    });
+}
+function getYodaSpeak() {
+    console.log("Getting yoda translation...");
+    $.ajax({
+        url: 'https://yoda.p.mashape.com/yoda?sentence=' + retrievedQuote,
+        type: 'GET',
+        datatype: 'json',
+        success: function (data) {
+            console.log(data);
+            translatedQuote = data;
+            console.log("Got the translation:" + translatedQuote);
+        },
+        error: function (err) {
+            alert(err);
+        },
+        beforeSend: function (xhr) {
+            xhr.setRequestHeader("X-Mashape-Authorization", "AwbgEZ1Wqqmshirsyf5x49g4qTj3p1KK0uHjsnwSyoj4AQc1p5"); // Enter here your Mashape key
+        }
+    });
+}
+function updateYodaSpeak() {
+    $("#final-quote").html(translatedQuote);
+}
---
+++ js/index.ts
@@ -0,0 +1,61 @@
+var retrievedQuote = ""
+var retrievedAuthor = ""
+var retrievedCategory = "movies"
+
+var translatedQuote = ""
+
+function generateQuote(): void {
+
+  
+  getQuote()
+
+}
+
+function getQuote(): void {
+  console.log("Getting quote...")
+  $.ajax({
+    url: "https://andruxnet-random-famous-quotes.p.mashape.com/?cat=" + retrievedCategory,
+    beforeSend: function (xhrObj) {
+      xhrObj.setRequestHeader("Accept", "application/json");
+      xhrObj.setRequestHeader("Content-Type", "application/x-www-form-urlencoded")
+      xhrObj.setRequestHeader("X-Mashape-Authorization", "AwbgEZ1Wqqmshirsyf5x49g4qTj3p1KK0uHjsnwSyoj4AQc1p5");
+    },
+    type: "POST"
+  })
+    .done(function (data) {
+
+      var json = JSON.parse(data);
+      retrievedQuote = json.quote;
+
+      getYodaSpeak()
+
+    })
+    .fail(function (error) {
+      console.log(error);
+    });
+
+}
+
+function getYodaSpeak(): void {
+  console.log("Getting yoda translation...")
+  $.ajax({
+    url: 'https://yoda.p.mashape.com/yoda?sentence=' + retrievedQuote,
+    type: 'GET',
+    datatype: 'json',
+    success: function (data) {
+
+      translatedQuote = data;
+
+    },
+    error: function (err) {
+      alert(err);
+    },
+    beforeSend: function (xhr) {
+      xhr.setRequestHeader("X-Mashape-Authorization", "AwbgEZ1Wqqmshirsyf5x49g4qTj3p1KK0uHjsnwSyoj4AQc1p5"); // Enter here your Mashape key
+    }
+  });
+}
+
+function updateYodaSpeak(): void {
+  $("#final-quote").html(translatedQuote);
+}
